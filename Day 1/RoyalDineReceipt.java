902cb67941075a5867b0e73a39642622207b5d5c
25BEEG04 <abhijitbiswal700@gmail.com>
2026-05-27 21:53:34 +0530

    Add files via upload

---
+++ Day 1/RoyalDineReceipt.java
@@ -0,0 +1,23 @@
+package java_internship_asst;
+
+public class RoyalDineReceipt {
+
+    public static void main(String[] args) {
+
+        System.out.println("======================================");
+        System.out.println("         ROYAL DINE RESTAURANT        ");
+        System.out.println("======================================");
+
+        System.out.println("\n------------- FOOD MENU -------------");
+        System.out.println(" Paneer Butter Masala   : ₹350");
+        System.out.println(" Veg Biryani            : ₹280");
+        System.out.println(" Cold Coffee            : ₹150");
+        System.out.println("--------------------------------------");
+
+        System.out.println(" Total Bill Amount      : ₹780");
+        System.out.println("--------------------------------------");
+
+        System.out.println("     Thank You! Visit Again :)");
+        System.out.println("======================================");
+    }
+}
