c0e16729721b4317ef85ba0d6dad9f1706db6f18
25BEEG04 <abhijitbiswal700@gmail.com>
2026-05-27 21:31:06 +0530

    Add files via upload

---
+++ Day 1/Personal_intro_updated.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package java_internship_asst;
+
+/**
+ * 
+ */
+public class Personal_intro {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("NAME : Abhijit Biswal");
+		System.out.println("Branch: EEE");
+		System.out.println("HOBBIES : Reading books");
+	}
+
+}
