4a6d5e89a6e55545462fe6811feb3d7235d3f187
25BEEG04 <abhijitbiswal700@gmail.com>
2026-05-28 21:41:55 +0530

    Add files via upload

---
+++ DAY 3/Bonus (1).java
@@ -0,0 +1,39 @@
+package JavaIgniteDay3;
+import java.util.Scanner;
+
+public class Bonus {
+	public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter number of working years:");
+			int year = sc.nextInt();
+			System.out.println("Enter the attendance (in %):");
+			double attendance = sc.nextDouble();
+			System.out.println("Enter performance rating (1-5):");
+			int rating = sc.nextInt();
+			
+			if(attendance>85 && year>3)
+			{
+				System.out.println("Eligible for bonus");
+				switch(rating)
+				{
+					case 5:
+						System.out.println("Bonus Rewarded : Rs. 50,000");
+						break;
+					case 4:
+						System.out.println("Bonus Rewarded : Rs. 30,000");
+						break;
+					case 3:
+						System.out.println("Bonus Rewarded : Rs. 10,000");
+						break;
+					default:
+						System.out.println("no bonus!");
+				}
+			}
+			else if(attendance<60)
+			{
+				System.out.println("Not eligible for bonus");
+			}
+			sc.close();
+	}
+
+}
---
+++ DAY 3/Shoppingdemo (1).java
@@ -0,0 +1,21 @@
+package JavaIgniteDay3;
+
+public class Shoppingdemo {
+	public static void main(String[] args) {
+		double amt = 10000;
+		boolean PremiumUser = true;
+		if(amt >= 5000 && PremiumUser)
+		{
+			System.out.println("30% Discount awarded! ");
+		}
+		else if(amt >= 5000 || PremiumUser)
+		{
+			System.out.println("10% Discount awarded! ");
+		}
+		else
+		{
+			System.out.println("No Discount! ");
+		}
+	}
+
+}
