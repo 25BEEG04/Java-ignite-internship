32f5aad6867761c5fd4adf04a6c1dc1932d7cdf7
25BEEG04 <abhijitbiswal700@gmail.com>
2026-05-30 20:51:38 +0530

    Add files via upload

---
+++ DAY 5/Travelexpense.java
@@ -0,0 +1,31 @@
+package JavaIgniteDay5;
+import java.util.Scanner;
+public class Travelexpense {
+	double travel_cost(double petrol_price,double mileage, double distance) {
+		double petrol=distance/mileage;
+		double travel_cost = petrol_price*petrol;
+		if(mileage == 0) {
+			System.out.println("Not Posible");
+			return 0;	
+		}
+		return travel_cost;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double result;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the distance travelled:");
+		double distance = sc.nextDouble();
+		System.out.println("Enter the mileage:");
+		double mileage = sc.nextDouble();
+		System.out.println("Enter the petrol price:");
+		double petrol_price = sc.nextDouble();
+		
+		Travelexpense price = new Travelexpense();
+		result=price.travel_cost(petrol_price, mileage, distance);
+		System.out.printf("Final amount needed for the trip is %.2f",result);
+		sc.close();
+	}
+
+}
